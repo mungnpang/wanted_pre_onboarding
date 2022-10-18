c033e2361bf11420fa92f1b25c9bb942a6b2bbec
mungnpang <koressma@gmail.com>
2022-10-18 20:13:07 +0900

    add - company & job_posting models

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from company.models import FundingStage as FundingStageModel
+from company.models import Company as CompanyModel
+
 # Register your models here.
+class FundingStage(admin.ModelAdmin):
+    list_display = ('id', 'funding_stage')
+    
+class Company(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    
+admin.site.register(FundingStageModel, FundingStage)
+admin.site.register(CompanyModel, Company)
