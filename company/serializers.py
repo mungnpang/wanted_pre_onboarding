0b866a644ae48c2efcc417499660adaa9f322240
mungnpang <koressma@gmail.com>
2022-10-19 15:07:16 +0900

    add - find other_posts function

--- company/serializers.py
+++ company/serializers.py
@@ -8,11 +8,19 @@ class FundingStageSerializer(serializers.ModelSerializer):
     class Meta:
         model = FundingStageModel
         fields = ["funding_stage"]
-        
+
 
 class CompanySerializer(serializers.ModelSerializer):
+    other_posts = serializers.SerializerMethodField()
     seed = FundingStageSerializer()
+    
+    def get_other_posts(self, obj):
+        posts = []
+        for post in obj.company.all():
+            posts.append(post.id)
+            
+        return {"이 회사의 다른 공고들": [posts]}
+    
     class Meta:
         model = CompanyModel
-        fields = ["name", "country", "region", "seed", "employees"]
-        
+        fields = ["name", "country", "region", "seed", "employees", "other_posts"]
